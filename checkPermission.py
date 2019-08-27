ce6c5712fe7db89d5112af52e622cb42471819d2
wangxupu <8062@qq.com>
2019-08-27 20:50:34 +0800

    change find method

--- checkPermission.py
+++ checkPermission.py
@@ -3,12 +3,14 @@ import zipfile
 import sys
 
 from libdex import dex
-import library
+#import library
 total_permission_list = []
 usage_perm_list = []
 lib_perm_list = []
 over_perm_list = []
 perm_map = {}
+lib_map = []
+library_list = []
 def get_permission(content):
     perm_list = content.split('\n')
     for perm_value in perm_list:
@@ -28,6 +30,12 @@ def get_method_perm(file_path):
             end = now_line[0].index('(')
             perm_map[now_line[0][0:end]] = now_line[1]
 
+def get_library_map(file_path):
+    f = open(file_path)
+    for lines in f.readlines():
+        now_line = lines.strip()[1:]
+        lib_map.append(now_line)
+
 def get_dex_file(filepath):
     permission_content = os.popen('./tools/aapt dump permissions {}'.format(filepath)).read()
     #print(permission_content)
@@ -43,13 +51,27 @@ def get_dex_file(filepath):
             dex_file = open(dexfilename,'wb+')
             dex_file.write(apkfile.read(tempfile))
             dex_info = dex.Dex(dexfilename)
-            library_list = library.detect_exact_dex_libraries(dex_info)
+            #library_list = library.detect_exact_dex_libraries(dex_info)
             if hasattr(dex_info, 'classes'):
                 for class_ in dex_info.classes:
-                    #print(class_.name())
+                   # print(class_.name())
                     tag = False
-                    if class_.name in library_list.keys():
-                        tag = True
+                    tmp_class_name = class_.name()[1:-1]
+                    for lib in lib_map:
+                        if tmp_class_name.startswith(lib):
+                            tag = True
+                            if lib not in library_list:
+                                library_list.append(lib)
+                            break
+                    new_class_name = tmp_class_name.replace('/','.')
+                    #print(new_class_name)
+                    for key in perm_map:
+                        if key.startswith(new_class_name) and perm_map[key] in total_permission_list and perm_map[key] not in usage_perm_list:
+                            usage_perm_list.append(perm_map[key])
+                        if tag and perm_map[key] in total_permission_list and perm_map[key] not in lib_perm_list:
+                            lib_perm_list.append(perm_map[key])
+                    #if class_.name in library_list.keys():
+                    #    tag = True
                     for method in class_.methods():
                         #print('method', method.name())
                         method_name = method.name().replace('/','.')
@@ -68,13 +90,18 @@ def get_dex_file(filepath):
         if perm not in usage_perm_list:
             over_perm_list.append(perm)
     print('total_permission_list',total_permission_list)
+    print('')
     print('true use permission',usage_perm_list)
+    print('')
     print('library',library_list)
+    print('')
     print('library permissiom usage',lib_perm_list)
+    print('')
     print('overprilege permission', over_perm_list)
 
 get_method_perm('./tools/framework-map-25.txt')
 get_method_perm('./tools/sdk-map-25.txt')
-#print(perm_map)
+get_library_map('./tools/libs.txt')
+#print(lib_map)
 get_dex_file(sys.argv[1])
 
